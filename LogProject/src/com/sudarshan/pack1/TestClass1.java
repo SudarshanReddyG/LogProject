cb32d665064aff50b318417a3ec206d3e546aa52
SudarshanReddyG <sudarshang.reddy@gmail.com>
2015-08-23 16:26:52 +0530

    Using LTW

--- LogProject/src/com/sudarshan/pack1/TestClass1.java
+++ LogProject/src/com/sudarshan/pack1/TestClass1.java
@@ -2,6 +2,7 @@ package com.sudarshan.pack1;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 import com.sudarshan.pack2.TestClass4;
 
@@ -18,12 +19,16 @@ public class TestClass1 {
 	public static void main(String[] args) throws Exception {
 		TestClass1 tc1 = new TestClass1();
 		
-
 		try {
 			JoranConfigurator configurator = new JoranConfigurator();
 			configurator.setContext(tc1.lc);
 			tc1.lc.reset();
-			configurator.doConfigure("E:/Spring/Spring Security/Eclipse Workspace/LogProject/logback.xml");
+			//configurator.doConfigure("E:/Spring/Spring Security/Eclipse Workspace/LogProject/logback.xml");
+			//configurator.doConfigure("D:/GitRepositories/LogProject/rollingFilelogback.xml");
+			configurator.doConfigure("D:/GitRepositories/LogProject/siftlogback.xml");
+			MDC.put("ifsc", "ABNK0000001");
+			//tc1.logger.debug("hello");
+			//MDC.remove("ifsc");
 		} catch (JoranException je) {
 			je.printStackTrace();
 		}
@@ -34,14 +39,16 @@ public class TestClass1 {
 		String name = "Sudarshan";
 		String system = "Dude";
 		Thread.currentThread().setName("TestClass1");
-		tc1.logger.debug("Hello(TestClass1) {} how are u? I am good {}",name, system);
-		//Thread.sleep(10000);
-		tc1.logger.info("Hello1(TestClass1) {} how are u? I am good {}",name, system);
-		//Thread.sleep(10000);
-		tc1.logger.error("Hello2(TestClass1) {} how are u? I am good {}",name, system);
+		for(int i=0;i<1000;i++) {
+			tc1.logger.debug("Hello(TestClass1) {} how are u? I am good {}",name, system);
+			//Thread.sleep(10000);
+			tc1.logger.info("Hello1(TestClass1) {} how are u? I am good {}",name, system);
+			//Thread.sleep(10000);
+			tc1.logger.error("Hello2(TestClass1) {} how are u? I am good {}",name, system);
 
-		new TestClass3().testMethod();
-		new TestClass4().testMethod();
+			new TestClass3().testMethod();
+			new TestClass4().testMethod();
+		}
 	}
 
 }
--- LogProject/src/com/sudarshan/pack1/TestClass2.java
+++ LogProject/src/com/sudarshan/pack1/TestClass2.java
@@ -5,16 +5,18 @@ import org.slf4j.LoggerFactory;
 
 public class TestClass2 {
 	
-	private static Logger logger = LoggerFactory.getLogger(TestClass2.class);
+	private Logger logger = LoggerFactory.getLogger(TestClass2.class);
 
 	public static void main(String[] args) throws Exception {
+		
+		TestClass2 testClass2Obj = new TestClass2();
 		String name = "Sudarshan";
 		String system = "Dude";
 		Thread.currentThread().setName("TestClass2");
-		logger.debug("Hello(TestClass2) {} how are u? I am good {}",name, system);
+		testClass2Obj.logger.debug("Hello(TestClass2) {} how are u? I am good {}",name, system);
 		//Thread.sleep(10000);
-		logger.info("Hello1(TestClass2) {} how are u? I am good {}",name, system);
+		testClass2Obj.logger.info("Hello1(TestClass2) {} how are u? I am good {}",name, system);
 		//Thread.sleep(10000);
-		logger.error("Hello2(TestClass2) {} how are u? I am good {}",name, system);
+		testClass2Obj.logger.error("Hello2(TestClass2) {} how are u? I am good {}",name, system);
 	}
 }
